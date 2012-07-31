8b1313c55cd87a70c891e1bc44951c8bbabaaf5b
Bruno Martins <bgcngm@gmail.com>
2012-07-31 19:31:30 +0100

    included support for MT65xx logo images

--- repack-MT65xx.pl
+++ repack-MT65xx.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl -W
+#!/usr/bin/perl
 
 #
 # script from Android-DLS WiKi
@@ -7,77 +7,160 @@
 #   - modified to work with MT6516 boot and recovery images (17-03-2011)
 #   - included support for MT65x3 and eliminated the need of header files (16-10-2011)
 #   - added cygwin mkbootimg binary and propper fix (17-05-2012)
+#   - included support for MT65xx logo images (31-07-2012)
 #
 
 use strict;
+use warnings;
 use Cwd;
-
+use Switch;
+use Compress::Zlib;
 
 my $dir = getcwd;
 
-my $usage = "repack-MT65xx.pl [-recovery] <kernel> <ramdisk-directory> <outfile>\n";
+my $version = "MTK-Tools by Bruno Martins\nMT65xx repack script (last update: 31-07-2012)\n";
+my $usage = "repack-MT65xx.pl COMMAND [...]\n\nCOMMANDs are:\n\n  -boot <kernel> <ramdisk-directory> <outfile>\n    Repacks boot image\n\n  -recovery <kernel> <ramdisk-directory> <outfile>\n    Repacks recovery image\n\n  -logo <logo-directory> <outfile>\n    Repacks logo image\n\n";
 
-# initilization
-my $kernel;
-my $ramdiskdir;
-my $outfile;
+print "$version";
+die "\nUsage: $usage" unless $ARGV[0] && $ARGV[1] && $ARGV[2];
 
-# set default header (rootfs signature)
-my $header = pack("H*","88168858a3910400524f4f5446530000000000000000000000000000000000000000000000000000ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
-
-die "\nUsage:\n\n$usage" unless $ARGV[0] && $ARGV[1] && $ARGV[2] && !$ARGV[4];
-if ( ( $ARGV[0] && $ARGV[1] && $ARGV[2] && $ARGV[3] && $ARGV[0] ne "-recovery" ) or ( $ARGV[0] eq "-recovery" && !$ARGV[3] ) ) {
-	die "\nUsage:\n\n$usage";
+switch($ARGV[0]) {
+	case "-boot" {
+		die "\nUsage: $usage" unless $ARGV[3] && !$ARGV[4];
+		repack_boot("ROOTFS");
+	}
+	case "-recovery" {
+		die "\nUsage: $usage" unless $ARGV[3] && !$ARGV[4];
+		repack_boot("RECOVERY");
+	}
+	case "-logo" {
+		die "\nUsage: $usage" unless !$ARGV[3];
+		repack_logo("LOGO");
+	}
+	else {
+		die "\nUsage: $usage"
+	}
 }
 
-if ( $ARGV[0] eq "-recovery" ) {
-	# change header (recovery signature)
-	$header = pack("H*","88168858a39104005245434f56455259000000000000000000000000000000000000000000000000ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
-	$kernel = $ARGV[1];
-	$ramdiskdir = $ARGV[2];
-	$outfile = $ARGV[3];
-} else {
-	$kernel = $ARGV[0];
-	$ramdiskdir = $ARGV[1];
-	$outfile = $ARGV[2];
+sub repack_boot {
+	# initilization
+	my $kernel = $ARGV[1];
+	my $ramdiskdir = $ARGV[2];
+	my $outfile = $ARGV[3];
+	my $signature = $_[0];
+	$ARGV[0] =~ s/-//;
+	
+	chdir $ramdiskdir or die "\n$ramdiskdir $!";
+
+	print "\nRepacking $ARGV[0] image...\nRamdisk size: ";
+	system ("find . | cpio -o -H newc | gzip > $dir/ramdisk-repack.cpio.gz");
+
+	chdir $dir or die "\n$ramdiskdir $!";;
+
+	my $slurpvar = $/;
+	undef $/;
+	open (RAMDISKFILE, "ramdisk-repack.cpio.gz") or die "Could not open ramdisk file: ramdisk-repack.cpio.gz\n";
+	my $ramdisk = <RAMDISKFILE>;
+	close RAMDISKFILE;
+	$/ = $slurpvar;
+
+	# generate the header according to the ramdisk size
+	my $sizeramdisk = length($ramdisk);
+	my $header = gen_header($signature, $sizeramdisk);
+
+	# attach the header to ramdisk
+	my $newramdisk = $header . $ramdisk;
+
+	open (RAMDISKFILE, ">new-ramdisk-repack.cpio.gz");
+	print RAMDISKFILE $newramdisk or die;
+	close RAMDISKFILE;
+
+	# create the output file
+	if ( $^O eq "cygwin" ) {
+		system ("./mkbootimg.exe --kernel $kernel --ramdisk new-ramdisk-repack.cpio.gz -o $outfile");
+	} else {
+		system ("mkbootimg --kernel $kernel --ramdisk new-ramdisk-repack.cpio.gz -o $outfile");
+	}
+
+	# cleanup
+	unlink("ramdisk-repack.cpio.gz") or die $!;
+	system("rm new-ramdisk-repack.cpio.gz");
+
+	print "\nRepacked $ARGV[0] image into $outfile\n";
 }
 
-chdir $ramdiskdir or die "$ramdiskdir $!";
+sub repack_logo {
+	# initilization
+	my $input;
+	my $logodir = $ARGV[1];
+	my $outfile = $ARGV[2];
+	my $signature = $_[0];
+	$ARGV[0] =~ s/-//;
+	
+	chdir $logodir or die "\n$logodir $!";
 
-system ("find . | cpio -o -H newc | gzip > $dir/ramdisk-repack.cpio.gz");
+	my (@raw_addr, @zlib_raw) = ();
+	print "\nRepacking $ARGV[0] image...\n";
 
-chdir $dir or die "$ramdiskdir $!";;
+	my $i = 0;
+	my $slurpvar = $/;
+	undef $/;
+	for my $inputfile ( glob "./*.rgb565" ) {
+		open (INPUTFILE, "$inputfile") or die "Could not open raw image: $inputfile\n";
+		$input = <INPUTFILE>;
+		close INPUTFILE;
 
-my $slurpvar = $/;
-undef $/;
-open (RAMDISKFILE, "ramdisk-repack.cpio.gz") or die "could not open ramdisk file: ramdisk-repack.cpio.gz\n";
-my $ramdisk = <RAMDISKFILE>;
-close RAMDISKFILE;
-$/ = $slurpvar;
+		# deflate all rgb565 raw files found (compress zlib rfc1950)
+		$zlib_raw[$i] = compress($input,Z_BEST_COMPRESSION);
 
-# update header according to the new ramdisk size
-my $sizeramdisk = -s 'ramdisk-repack.cpio.gz';
-my $hexsizeramdisk = sprintf("%08X", $sizeramdisk);
-$header = pack("a4 H8", substr($header,0,4), substr($hexsizeramdisk,6) . substr($hexsizeramdisk,4,-2) . substr($hexsizeramdisk,2,-4) . substr($hexsizeramdisk,0,-6)) . substr($header,8);
-my $newramdisk = $header . $ramdisk;
+		$i++;
+	}
+	die "Could not find any .rgb565 file under the specified folder: $logodir\n" unless $i > 0;
+	
+	$/ = $slurpvar;
+	my $num_blocks = $i;
+	print "Number of raw images found in the specified folder: $num_blocks\n";
+	
+	my $logo_length = (4 + 4 + $num_blocks * 4);
+	# calculate the start address of each raw image and the new file size
+	$i = 0;
+	do {
+		$raw_addr[$i] = $logo_length;
+		$logo_length += length($zlib_raw[$i]);
+		$i++;
+	} while $i < $num_blocks;
 
-open (RAMDISKFILE, ">new-ramdisk-repack.cpio.gz");
-print RAMDISKFILE $newramdisk or die;
-close RAMDISKFILE;
+	# generate logo header according to the logo size
+	my $logo_header = gen_header($signature, $logo_length);
+	
+	my $logobin = pack('LL', $num_blocks, $logo_length);
+	
+	$i = 0;
+	do {
+		$logobin .= pack('L', $raw_addr[$i]);
+		$i++;
+	} while $i < $num_blocks;
 
-# create the outfile
-if ( $^O eq "cygwin" ) {
-	system ("./mkbootimg.exe --kernel $kernel --ramdisk new-ramdisk-repack.cpio.gz -o $outfile");
-} else {
-	system ("mkbootimg --kernel $kernel --ramdisk new-ramdisk-repack.cpio.gz -o $outfile");
+	$i = 0;
+	do {
+		$logobin .= $zlib_raw[$i];
+		$i++;
+	} while $i < $num_blocks;
+
+	$logobin = $logo_header . $logobin;
+
+	# create the output file
+	open (RAWFILE, ">../$outfile");
+	binmode(RAWFILE);
+	print RAWFILE $logobin or die;
+	close RAWFILE;
+
+	print "\nRepacked $ARGV[0] image into $outfile\n";
 }
 
-# cleanup
-unlink("ramdisk-repack.cpio.gz") or die $!;
-system("rm new-ramdisk-repack.cpio.gz");
+sub gen_header {
+	my $header_type = $_[0];
+	my $length = $_[1];
 
-if ( $ARGV[0] eq "-recovery" ) {
-	print "\nrepacked recovery image written to $outfile\n";
-} else {
-	print "\nrepacked boot image written to $outfile\n";
+	return pack('a4La32H*', "\x88\x16\x88\x58", $length, $header_type, "ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
 }
--- unpack-MT65xx.pl
+++ unpack-MT65xx.pl
@@ -1,67 +1,137 @@
-#!/usr/bin/perl -W
+#!/usr/bin/perl
 
+#
 # script from Android-DLS WiKi
 #
 # changes by Bruno Martins:
 #   - modified to work with MT6516 boot and recovery images (17-03-2011)
 #   - included support for MT65x3 and eliminated the need of header files (16-10-2011)
+#   - added cygwin mkbootimg binary and propper fix (17-05-2012)
+#   - included support for MT65xx logo images (31-07-2012)
+#
 
 use strict;
+use warnings;
 use bytes;
 use File::Path;
+use Compress::Zlib;
 
-my $usage = "unpack-MT65xx.pl <image>\n";
+my $version = "MTK-Tools by Bruno Martins\nMT65xx unpack script (last update: 31-07-2012)\n";
+my $usage = "unpack-MT65xx.pl <infile>\n  Unpacks boot, recovery or logo image\n\n";
 
-die "\nUsage:\n\n$usage" unless $ARGV[0];
+print "$version";
+die "\nUsage: $usage" unless $ARGV[0];
 
-my $bootimgfile = $ARGV[0];
+my $inputfile = $ARGV[0];
 
 my $slurpvar = $/;
 undef $/;
-open (BOOTIMGFILE, "$bootimgfile") or die "could not open boot img file: $bootimgfile\n";
-my $bootimg = <BOOTIMGFILE>;
-close BOOTIMGFILE;
+open (INPUTFILE, "$inputfile") or die "\nCould not open the specified file: $inputfile\n";
+my $input = <INPUTFILE>;
+close INPUTFILE;
 $/ = $slurpvar;
 
-my($bootMagic, $kernelSize, $kernelLoadAddr, $ram1Size, $ram1LoadAddr, $ram2Size, $ram2LoadAddr, $tagsAddr, $pageSize, $unused1, $unused2, $bootName, $cmdLine, $id) = unpack('a8 L L L L L L L L L L a16 a512 a8', $bootimg);
-
-my($kernel) = substr($bootimg, $pageSize, $kernelSize);
-
-open (KERNELFILE, ">$ARGV[0]-kernel.img");
-binmode(KERNELFILE);
-print KERNELFILE $kernel or die;
-close KERNELFILE;
-
-print "\nkernel written to $ARGV[0]-kernel.img\n";
-
-my($kernelAddr) = $pageSize;
-my($kernelSizeInPages) = int(($kernelSize + $pageSize - 1) / $pageSize);
-
-my($ram1Addr) = (1 + $kernelSizeInPages) * $pageSize;
-
-my($ram1) = substr($bootimg, $ram1Addr, $ram1Size);
-
-# chop ramdisk header
-$ram1 = substr($ram1, 512);
-
-if (substr($ram1, 0, 2) ne "\x1F\x8B") {
-	die "the boot image does not appear to contain a valid gzip file";
+if ((substr($input, 0, 4) eq "\x88\x16\x88\x58") & (substr($input, 8, 4) eq "LOGO")) {
+	# if the input file contains the logo signature, try to unpack it
+	print "\nValid logo signature found...\n";
+	unpack_logo($input);
+} elsif (substr($input, 0, 7) eq "\x41\x4e\x44\x52\x4f\x49\x44") {
+	# else, a valid Android signature is found, try to unpack boot or recovery image
+	print "\nValid Android signature found...\n";
+	unpack_boot($input);
+} else {
+	die "\nThe input file does not appear to be a valid file.";
 }
 
-open (RAMDISKFILE, ">$ARGV[0]-ramdisk.cpio.gz");
-binmode(RAMDISKFILE);
-print RAMDISKFILE $ram1 or die;
-close RAMDISKFILE;
+sub unpack_boot {
+    my $bootimg = $_[0];
+	my($bootMagic, $kernelSize, $kernelLoadAddr, $ram1Size, $ram1LoadAddr, $ram2Size, $ram2LoadAddr, $tagsAddr, $pageSize, $unused1, $unused2, $bootName, $cmdLine, $id) = unpack('a8 L L L L L L L L L L a16 a512 a8', $bootimg);
 
-print "\nramdisk written to $ARGV[0]-ramdisk.cpio.gz\n";
+	my($kernel) = substr($bootimg, $pageSize, $kernelSize);
 
-if (-e "$ARGV[0]-ramdisk") {
-        rmtree "$ARGV[0]-ramdisk";
-        print "\nremoved old directory $ARGV[0]-ramdisk\n";
+	open (KERNELFILE, ">$ARGV[0]-kernel.img");
+	binmode(KERNELFILE);
+	print KERNELFILE $kernel or die;
+	close KERNELFILE;
+
+	print "\nKernel written to $ARGV[0]-kernel.img\n";
+
+	my($kernelAddr) = $pageSize;
+	my($kernelSizeInPages) = int(($kernelSize + $pageSize - 1) / $pageSize);
+
+	my($ram1Addr) = (1 + $kernelSizeInPages) * $pageSize;
+
+	my($ram1) = substr($bootimg, $ram1Addr, $ram1Size);
+
+	# chop ramdisk header
+	$ram1 = substr($ram1, 512);
+
+	if (substr($ram1, 0, 2) ne "\x1F\x8B") {
+		die "\nThe boot image does not appear to contain a valid gzip file";
+	}
+
+	open (RAMDISKFILE, ">$ARGV[0]-ramdisk.cpio.gz");
+	binmode(RAMDISKFILE);
+	print RAMDISKFILE $ram1 or die;
+	close RAMDISKFILE;
+
+	print "Ramdisk written to $ARGV[0]-ramdisk.cpio.gz\n";
+
+	if (-e "$ARGV[0]-ramdisk") {
+		rmtree "$ARGV[0]-ramdisk";
+		print "Removed old ramdisk directory $ARGV[0]-ramdisk\n";
+	}
+
+	mkdir "$ARGV[0]-ramdisk" or die;
+	chdir "$ARGV[0]-ramdisk" or die;
+	print "Ramdisk size: ";
+	system ("gzip -d -c ../$ARGV[0]-ramdisk.cpio.gz | cpio -i");
+
+	print "Extracted ramdisk contents to directory $ARGV[0]-ramdisk\n";
+	print "\nSuccessfully unpacked kernel and ramdisk.\n";
 }
 
-mkdir "$ARGV[0]-ramdisk" or die;
-chdir "$ARGV[0]-ramdisk" or die;
-system ("gzip -d -c ../$ARGV[0]-ramdisk.cpio.gz | cpio -i");
+sub unpack_logo {
+    my $logobin = $_[0];
+	# get logo header
+	my $header = substr($logobin, 0, 512);
+	my ($header_sig, $logo_length, $logo_sig) = unpack('a4 V A4', $header);
 
-print "\nextracted ramdisk contents to directory $ARGV[0]-ramdisk\n";
+	# just one more check to make sure that the logo image is valid
+	my $sizelogobin = -s $inputfile;
+	if ($logo_length != $sizelogobin - 512) {
+		die "The logo image does not appear to be valid.";
+	}
+
+	# chop the header and extract logo information
+	my $logo = substr($logobin, 512);
+	my $num_blocks = unpack('V', $logo);
+
+	my $i = 0;
+	my (@raw_addr, @zlib_raw) = ();
+	print "\nNumber of raw images found: $num_blocks\n";
+	# get the starting address of each raw file
+	do {
+		$raw_addr[$i] = unpack('L', substr($logo, 8+$i*4, 4));
+		$i++;
+	} while $i < $num_blocks;
+
+	$i = 0;
+	# extract rgb565 raw files (uncompress zlib rfc1950)
+	do {
+		if ($i < $num_blocks-1) {
+			$zlib_raw[$i] = substr($logo, $raw_addr[$i], $raw_addr[$i+1]-$raw_addr[$i]);
+		} else {
+			$zlib_raw[$i] = substr($logo, $raw_addr[$i]);
+		}
+		open (RAWFILE, ">$ARGV[0]-raw[$i].rgb565");
+		binmode(RAWFILE);
+		print RAWFILE uncompress($zlib_raw[$i]) or die;
+		close RAWFILE;
+		print "Raw image #$i written to $ARGV[0]-raw[$i].rgb565\n";
+
+		$i++;
+	} while $i < $num_blocks;
+
+	print "\nSuccessfully extracted all raw images.\n";
+}
